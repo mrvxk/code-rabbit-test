71f7efd7e997087938b53044891d0891ca8ea866
mrvxk <kleinermarvin@icloud.com>
2024-04-05 10:46:47 +0200

    Init sprint boot

---
+++ src/test/java/com/mrvxk/coderabbittest/CodeRabbitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrvxk.coderabbittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRabbitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
